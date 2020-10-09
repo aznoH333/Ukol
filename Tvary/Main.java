7b70b30133dfe3936a453a754eb737b2e9a05090
aznoH333 <62841387+aznoH333@users.noreply.github.com>
2020-10-09 13:16:02 +0200

    do the tvars

--- Tvary/Main.java
+++ Tvary/Main.java
@@ -3,10 +3,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Octahedron jed = new Octahedron("Octahedron1",1);
+		Octahedron jed2 = new Octahedron("Octahedron2",2);
+		Octahedron jed3 = new Octahedron("Octahedron3",3);
 		
 		//pomoc pracuju v eclipse
 		jed.povrch();
 		jed.objem();
+		jed2.povrch();
+		jed2.objem();
+		jed3.povrch();
+		jed3.objem();
 	}
 
 }
