b4b19656908a5e9890320e6e60f04b19ebdb87f5
aznoH333 <62841387+aznoH333@users.noreply.github.com>
2021-01-18 14:55:31 +0100

    =

--- Pipe/ext.pipe.ts
+++ Pipe/ext.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ExtPipe implements PipeTransform {
 
   transform(value: String, a: number, s : String): String {
-    if (value.length < a - s.length){
+    if (value.length <= a - s.length){
       return value
     }else {
       return value.substr(0,a - s.length) + s;
